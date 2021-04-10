3cb1615b01bafcc0c1e5b516b3a28e11997a897e
Azzahry Dwi Pramudio <80077443+Azzahry@users.noreply.github.com>
2021-04-10 23:43:31 +0700

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,97 @@
+
+package inventory;
+
+
+public class Product {
+
+    //Instance field declarations
+    private int item;
+    private String name;
+    private int qty;
+    private double price;
+    private boolean active = true;
+    private double inventoryValue;
+    
+    public Product(){
+        
+    }
+    public Product (int item, String name, int qty, double price){
+        this.item = item;
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
+    
+    public int getItem() {
+        return item;
+    }
+
+    public void setItem(int item) {
+        this.item = item;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String nama) {
+        this.name = nama;
+    }
+    
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int stock) {
+        this.qty = stock;
+    }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public boolean getActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public double getInventoryValue() {
+        return inventoryValue;
+    }
+
+    public void setInventoryValue(double inventoryValue) {
+        this.inventoryValue = inventoryValue;
+    }
+    
+    
+    
+    @Override
+    public String toString(){
+        
+        String viewProduct;
+        viewProduct = "Item number :"+ getItem()+
+                      "\nName :"+ getName()+
+                      "\nQuantity in Stock :"+ getQty()+
+                      "\nPrice :"+ getPrice()+
+                      "\nStock Value :"+ getInventoryValue()+
+                      "\nProduct Status : Active ("+ getActive()+")";
+        
+        return viewProduct;
+    }
+    public String toString2(){
+        
+        String viewProduct;
+        viewProduct = "Item number :"+ getItem()+
+                      "\nName :"+ getName()+
+                      "\nQuantity in Stock :"+ getQty()+
+                      "\nPrice :"+ getPrice()+
+                      "\nStock Value :"+ getInventoryValue()+
+                      "\nProduct Status : Discontinue ("+getActive()+")";
+        
+        return viewProduct;
+    }
+}
+
---
+++ ProductTester.java
@@ -0,0 +1,120 @@
+
+package inventory;
+
+import java.util.Scanner;
+public class ProductTester {
+    
+    ProductTester(){
+        Product pro1 = new Product();
+        pro1.setItem(1);
+        pro1.setName("Steelseries Rival 3");
+        pro1.setQty(25);
+        pro1.setInventoryValue(999*25);
+        pro1.setPrice(99.9);
+        System.out.println(pro1.toString());
+        System.out.println("\n");
+        
+        pro1.setItem(2);
+        pro1.setName("Razer Basilisk pro V2");
+        pro1.setQty(21);
+        pro1.setInventoryValue(999*21);
+        pro1.setPrice(99.9);
+        System.out.println(pro1.toString());
+        System.out.println("\n");
+        
+    }
+    ProductTester(String Logitech, String Rexus, String Cougar, String Corsair){
+        Product pro2 = new Product();
+        pro2.setItem(3);
+        pro2.setName("Logitech G310");
+        pro2.setQty(25);
+        pro2.setInventoryValue(999*25);
+        pro2.setPrice(99.9);
+        System.out.println(pro2.toString());
+        System.out.println("\n");
+        
+        pro2.setItem(4);
+        pro2.setName("Rexus rx108");
+        pro2.setQty(21);
+        pro2.setInventoryValue(999*21);
+        pro2.setPrice(99.9);
+        System.out.println(pro2.toString());
+        System.out.println("\n");
+        
+        pro2.setItem(5);
+        pro2.setName("Cougar 250 M");
+        pro2.setQty(25);
+        pro2.setInventoryValue(999*25);
+        pro2.setInventoryValue(999*25);
+        pro2.setPrice(99.9);
+        System.out.println(pro2.toString());
+        System.out.println("\n");
+        
+        pro2.setItem(6);
+        pro2.setName("Corsair M65 RGB");
+        pro2.setQty(21);
+        pro2.setActive(false);
+        pro2.setInventoryValue(999*25);
+        pro2.setPrice(99.9);
+        
+        System.out.println(pro2.toString2());
+        System.out.println("\n");
+        
+        
+    }
+
+       
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ProductTester pt = new ProductTester();
+        ProductTester pt2 = new ProductTester("Logitech", "Rexus" , "Cougar" , "Corsair");
+        int tempNumber;
+        String tempName;
+        int tempQty;
+        double tempPrice;
+        int maxSize;
+        
+        System.out.println("====================================================");
+        System.out.println("Enter the number of product  you would like to add");
+        System.out.println("Enter zero (0) if you don't wish to add products");
+        System.out.println("====================================================\n");
+        int i = 10;
+        while(0 < i){
+            System.out.println("================================================");
+            System.out.println("Input number of product   :");
+            maxSize = in.nextInt();
+            
+            if(maxSize == 0){
+                System.out.println("Incorrect Value Entered");
+                System.out.println("No products required");
+                break;
+            }else{
+                String[] productBased;
+                for(int j = 0; j < maxSize; j++){
+                    System.out.print("Item Number       : ");
+                    tempNumber = in.nextInt(); 
+                    System.out.println("Product Name    : ");
+                    tempName = in.nextLine();
+                    System.out.print("Quantity          : ");
+                    tempQty = in.nextInt();
+                    System.out.print("Price             : ");
+                    tempPrice = in.nextDouble();
+                    System.out.println("=========================================");
+                    System.out.println("Item number         : "+ tempNumber);
+                    System.out.println("Product Name        : "+ tempName);
+                    System.out.println("Quantity in stock   : "+ tempQty);
+                    System.out.println("Product Price       : "+ tempPrice);
+                    
+                }
+            }
+            break;
+        }
+        
+        
+        
+        
+        
+}
+    
+    
+}
